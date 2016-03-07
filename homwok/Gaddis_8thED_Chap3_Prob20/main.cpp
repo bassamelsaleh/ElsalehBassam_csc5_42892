b184e338a02aefd706fdfb5a437ebd402103f274
bassamelsaleh <cpvpandrew@gmail.com>
2016-03-07 11:01:02 -0800

    Signed-off-by: bassamelsaleh <cpvpandrew@gmail.com>

---
+++ homwok/Gaddis_8thED_Chap3_Prob20/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * Author: Bassam Elsaleh
+ * 3/7/2016
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//user libraries
+
+//Global Constants
+const float PI = atan(1.0)*4;
+
+int main(int argc, char** argv) {
+    //declare variables
+    float sarea, area ;
+    int slice, diam;
+    
+    //set variables
+    sarea = 14.125;
+    diam= 12;
+    
+    //calculate
+    area = PI*(diam*diam)/4;
+    slice = static_cast<int>(area)/static_cast<int>(sarea);
+    
+            
+
+    return 0;
+}
+
---
+++ homwok/Gaddis_8thEd_Chap3_Prob19/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * Author: Bassam Elsaleh
+ * 3/7/2016
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//user libraries
+
+//Global Constants
+
+int main(int argc, char** argv) {
+    //declare variables
+    int  loan;
+    float mPay, rate,payment, top, bot, temp, chB;
+    
+    //Input variables
+    loan = 10000;
+    mPay = 36;
+    rate = 0.01;
+    temp = pow(1+rate, mPay);
+    top = rate*temp;
+    bot = temp-1;
+    
+    //calculation  
+    payment = top/bot*static_cast<float>(loan);
+    
+    //output results
+    cout<<"you monthly payment is $"<<payment<<endl;
+    
+    //equation for cheek back
+    chB=payment*(temp-1)/(temp*rate);
+    
+    //output results--check math should be 10000 with the set variables  
+    cout<<"you loan should be $"<<chB<<endl;
+    
+    
+    
+
+    return 0;
+}
+
