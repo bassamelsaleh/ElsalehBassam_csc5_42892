59c96235a8c9a03c2d99acd4e1b097be0290c951
bassamelsaleh <cpvpandrew@gmail.com>
2016-03-16 10:57:32 -0700

    Signed-off-by: bassamelsaleh <cpvpandrew@gmail.com>

--- class/Names/main.cpp
+++ class/Names/main.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <cstring>
+#include <string>
 
 using namespace std;
 
@@ -18,20 +19,43 @@ int main(int argc, char** argv) {
     char a[SIZE], b[SIZE], c[SIZE];
     
     //input values
-    cout<<"enter first name.";
+    cout<<"enter first name."<<endl;
     cin.getline(a,SIZE);
-    cout<<"enter second name.";
+    cout<<"enter second name."<<endl;
     cin.getline(b,SIZE);
-    cout<<"enter third name.";
+    cout<<"enter third name."<<endl;
     cin.getline(c,SIZE);
     
     //map the inputs to the outputs
-    if (strcmp(a, b)== 0)
+    if (strcmp(a,b)==0)
     {
-        if(strcmp(b, c)==0)
-   
-        cout<<a<<" "<<b<<" "<<c<<endl;
+        if (strcmp(c,b)==0)
+        cout<<a<<". "<<b<<". "<<c<<endl;
     }
+    else if (strcmp(a, b)< 0)
+    {
+        if(strcmp(b,c)<0)
+        cout<<a<<". "<<b<<". "<<c<<endl;
+        else if (strcmp(c,b)<0)
+            cout<<a<<". "<<c<<". "<<b<<endl;
+    }
+    else if (strcmp(b,a)<0)
+    {
+        if(strcmp(a,c)<0)
+        cout<<b<<". "<<a<<". "<<c<<endl;
+        else if (strcmp(c,a)<0)
+            cout<<b<<". "<<c<<". "<<a<<endl;
+    }
+        else if (strcmp(c,a))
+        {
+            if(strcmp(a,b)<0)
+        cout<<c<<". "<<a<<". "<<b<<endl;
+        else if (strcmp(b,a)<0)
+            cout<<c<<". "<<b<<". "<<a<<endl;
+        }
+    
+
+    
     //output the results
     
             
