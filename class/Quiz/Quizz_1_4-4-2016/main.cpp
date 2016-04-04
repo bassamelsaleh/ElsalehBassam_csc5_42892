f230eea547a32007c4a2adc90e04f2fd677dd059
bassamelsaleh <cpvpandrew@gmail.com>
2016-04-04 07:58:45 -0700

    Signed-off-by: bassamelsaleh <cpvpandrew@gmail.com>

---
+++ class/Quiz/Quizz_1_4-4-2016/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * Author: Bassam Elsaleh
+ * Purpose: for count
+ * Created on April 4, 2016, 7:47 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int sum;
+    int a=0;
+    for(sum=0;sum<=10;sum++){
+      a=a+sum;  
+    }
+    cout<<"the sum from 1 to 10 is ="<<a<<endl;
+
+    return 0;
+}
+
