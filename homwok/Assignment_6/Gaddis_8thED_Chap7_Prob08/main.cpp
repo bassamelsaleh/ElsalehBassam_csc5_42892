166d55620dbbe6d0b1dfece6073ee1aacd614503
bassamelsaleh <cpvpandrew@gmail.com>
2016-05-11 08:09:31 -0700

    Signed-off-by: bassamelsaleh <cpvpandrew@gmail.com>

--- homwok/Assignment_6/Gaddis_8thED_Chap7_Prob08/main.cpp
+++ homwok/Assignment_6/Gaddis_8thED_Chap7_Prob08/main.cpp
@@ -7,7 +7,6 @@
 
 #include <iostream>
 #include <iomanip>
-#include <fstream>
 
 using namespace std;
 
@@ -23,6 +22,11 @@ int main(int argc, char** argv) {
         int tot=0;
         int tot2=0;
         int tot3=0;
+        int tot4=0;
+        int tot5=0;
+        int tot6=0;
+        int tot7=0;
+        int tot8=0;
     
     int one[ROW][COL]={{4,9,2},{3,5,7},{8,1,6}};
     for(int count=0; count<ROW; count++){
@@ -32,25 +36,58 @@ int main(int argc, char** argv) {
        cout<<endl;
     }
     
-    
+    //row one
     for(int count=0; count<ROW; count++){
         tot+=one[1][count];
     }
-    
     cout<<"Row one added is = "<<tot<<endl;
     
+    //row two
     for(int count=0; count<ROW; count++){
         tot2+=one[2][count];
     }
-    
     cout<<"Row two added is = "<<tot2<<endl;
     
+    //row three
     for(int count=0; count<ROW; count++){
         
         tot3+=one[2][count];
     }
-    
     cout<<"Row three added is = "<<tot3<<endl;
+    
+    // diagonal one
+    for(int count=0, sec=0; count<ROW; count++, sec++){
+        
+        tot4+=one[sec][count];
+    }
+    cout<<"Row diagonal top left to bottom right added is = "<<tot4<<endl;
+    
+    
+    //diagonal two
+    for(int count=0,sec=2; count<ROW; count++, sec--){
+        
+        tot5+=one[count][sec];
+    }
+    cout<<"Row diagonal bottom left to top right added is = "<<tot5<<endl;
+    
+    
+    //columns one
+    for(int count=0; count<ROW; count++){
+        tot6+=one[count][0];
+    }
+    cout<<"Columns one added is = "<<tot<<endl;
+    
+    //columns two
+    for(int count=0; count<ROW; count++){
+        tot7+=one[count][1];
+    }
+    cout<<"Columns two added is = "<<tot2<<endl;
+    
+    //columns two
+    for(int count=0; count<ROW; count++){
+        tot8+=one[count][2];
+    }
+    cout<<"Columns three added is = "<<tot3<<endl;
     
     
     
